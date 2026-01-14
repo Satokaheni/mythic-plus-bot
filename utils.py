9683eb95360dc0ad70f7b6f771462003a06e9fbf
Satokaheni <zach.stallbohm@gmail.com>
2026-01-14 17:32:10 -0600

    updated with role validity checks

--- utils.py
+++ utils.py
@@ -7,6 +7,10 @@ from discord import Message
 message: str = """
 Please sign up for the following slots by reacting to the time slot you want to be in.
 If you have not declared a role with your class and roles you can play (in order of prevelance using the !role command) you will not be considered for a spot
+FOR DEATH KNIGHTS AND DEMON HUNTERS please use dk and dh respectively. 
+Example for the !role command
+!role paladin healer tank
+
 All runs will be based on completion for vault. The goal is not to push especially early in the tier. 
 If you react to line up that is already full you will be added as an extra if one of them cannot make it. 
 If you cannot make it to the run please remove your reaction from the schedule. 
@@ -17,7 +21,7 @@ These are for your raid toons so please do not fill out a role you cannot play.
 Reminders will be sent out 1 and 2 hours before start. 
 """
 
-def create_schedules() -> List[Schedule]:
+def create_schedules() -> List[Schedule]:FD
     def raid_day():
         return [time(17, 0), time(18, 0), time(23, 0)]
     
